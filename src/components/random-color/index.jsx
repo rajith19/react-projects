5b63297aa32694c8a776868580ab4ecf800dd9e1
rajith27 <rajithvgopalm@gmail.com>
2024-05-23 18:20:00 -0400

    style changes

--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -36,7 +36,7 @@ export default function RandomColor() {
   return (
     <div
       style={{
-        width: "100vw",
+        width: "100%",
         height: "100vh",
         background: color,
       }}
